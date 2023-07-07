d8f7cd4c4d567cddd79a0198cd66ab0da4e630e9
Camilla <milla_mendes83@hotmail.com>
2023-07-07 13:34:17 -0300

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,38 @@
 import React, { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { HashLink } from "react-router-hash-link";
+import {v4 as uuidv4} from 'uuid';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import "./App.css";
 
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import Header from "./components/Header";
-import TaskDetails from "./components/TaskDetails";
 
 const App = () => {
+
   const [tasks, setTasks] = useState([
     {
       id: "1",
       title: "Estudar Programação",
-      completed: false,
+      completed: true,
     },
     {
       id: "2",
       title: "Ler livros",
-      completed: true,
+      completed: false,
     },
   ]);
 
   const handleTaskRemove = (taskId) => {
-    const newTasks = tasks.filter((task) => task.id !== taskId);
-    setTasks(newTasks);
-  };
+    const newTasks = tasks.filter(task => task.id !== taskId);
+    setTasks(newTasks);  
+  }
 
   const handleTaskClick = (taskId) => {
     const newTasks = tasks.map((task) => {
-      if (task.id === taskId)
-        return {
-          ...task,
-          completed: !task.completed,
-        };
+      if(task.id === taskId) return {
+        ...task, completed: !task.completed
+      }
       return task;
     });
     setTasks(newTasks);
@@ -43,12 +40,11 @@ const App = () => {
 
   const handleTaskAddition = (taskTitle) => {
     const newTasks = [
-      ...tasks,
-      {
+      ...tasks, {
         title: taskTitle,
         id: uuidv4(),
         completed: false,
-      },
+      }
     ];
     setTasks(newTasks);
   };
@@ -57,36 +53,17 @@ const App = () => {
     <Router>
       <div className="container">
         <Header />
-        <ul>
-          <li>
-            <Link to="/ToDoList-projetoReact">Home</Link>
-          </li>
-          <li>
-            <HashLink smooth to="/ToDoList-projetoReact#add-task">
-              Add Task
-            </HashLink>
-          </li>
-        </ul>
-        <Route
-          path="/ToDoList-projetoReact"
-          exact
-          render={() => (
-            <>
-              <div id="add-task">
-                <AddTask handleTaskAddition={handleTaskAddition} />
-              </div>
-              <Tasks
-                tasks={tasks}
-                handleTaskClick={handleTaskClick}
-                handleTaskRemove={handleTaskRemove}
-              />
-            </>
-          )}
+        <Route path="/ToDoList-projetoReact" exact render={() => (
+          <>
+            <AddTask handleTaskAddition={handleTaskAddition} />
+            <Tasks tasks={tasks} 
+            handleTaskClick={handleTaskClick} handleTaskRemove= {handleTaskRemove} />
+          </>
+        )}
         />
-        <Route path="/task/:taskTitle" exact component={TaskDetails} />
       </div>
     </Router>
-  );
+  );    
 };
 
 export default App;
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import { CgClose, CgInfo } from "react-icons/cg"
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { CgClose } from "react-icons/cg"
 import "./Task.css";
 
 const Task = ({ task, handleTaskClick, handleTaskRemove}) => {
-  const history = useHistory(); 
-
-  const handleTaskDetailsClick = () => {
-    history.push(`/${task.title}`)
-  }
 
   return (
     <div className="task-container" style={task.completed ? {borderLeft: "6px solid #8e37a3" } : {} }
@@ -20,9 +14,6 @@ const Task = ({ task, handleTaskClick, handleTaskRemove}) => {
       <div className="buttons-container">
         <button className="remove-task-button" onClick={() => handleTaskRemove(task.id)}>
           <CgClose />
-        </button>
-        <button className="see-details-task-button" onClick={handleTaskDetailsClick}>
-          <CgInfo />
         </button>
       </div>
     </div>
